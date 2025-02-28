d47b53795400c7df88a089d3ffbade444c18baba
pranav1034 <pranavaggarwal04@gmail.com>
2025-02-28 11:05:26 +0530

    [Pranav] ADD Km-to-miles

---
+++ ConvertMiles.java
@@ -0,0 +1,7 @@
+public class ConvertMiles{
+	public static void main(String[] args){
+	double distanceKm= 10.8;
+	double distanceMiles = 10.8 * 1.6;
+	System.out.print("The distance "+distanceKm +" km in miles is "+distanceMiles);
+	}
+}
