d622e59b33145a330a8efff047c1ae426389edeb
pranav1034 <pranavaggarwal04@gmail.com>
2025-02-28 10:58:56 +0530

    [Pranav] add Average-marks

---
+++ AverageMarks.java
@@ -0,0 +1,10 @@
+public class AverageMarks{
+	public static void main(String[] args){
+	int maths= 94;
+	int physics=95;
+	int chemistry=96;
+	int totalSubjects=3;
+	int average= (physics+maths+chemistry)/totalSubjects ;
+	System.out.print("Sam's average marks in PCM is " + average);	
+	}
+}
