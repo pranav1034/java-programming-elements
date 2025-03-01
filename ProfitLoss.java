064c641688eed1a7d886543860dbac93056331eb
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-01 08:02:28 +0530

    [Pranav] ADD Profit-loss

---
+++ ProfitLoss.java
@@ -0,0 +1,8 @@
+public class ProfitLoss {
+    public static void main(String[] args) {
+        int costPrice = 129, sellingPrice = 191;
+        int profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / (double) costPrice) * 100;
+        System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice + "\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage + "%");
+    }
+}
